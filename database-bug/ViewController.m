05f60e32826adca891dd2027c684d052732c9f70
russellwheatley <russellwheatley85@gmail.com>
2023-12-28 11:41:39 +0000

    reproduction

--- database-bug/ViewController.m
+++ database-bug/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+@import Firebase;
 
 @interface ViewController ()
 
@@ -17,6 +18,62 @@
   [super viewDidLoad];
   // Do any additional setup after loading the view.
 }
+- (IBAction)writedata:(id)sender {
+  // Reference to the Firebase Database
+  FIRDatabaseReference *ref = [[FIRDatabase database] reference];
 
+  // Data to be written
+  NSDictionary *data = @{
+      @"organizations": @[
+          @{
+              @"city": @"Lyon",
+              @"country": @"France",
+              @"data": @[@"a", @"b", @"c", @"d", @"e", @"f", @"g", @"h"],
+              @"foo": @"bar",
+              @"id": @0,
+              @"name": @"my organization"
+          }
+      ]
+  };
+
+  // Write data to Firebase
+  [ref setValue:data withCompletionBlock:^(NSError * _Nullable error, FIRDatabaseReference * _Nonnull ref) {
+      if (error) {
+          NSLog(@"Data could not be saved: %@", error.localizedDescription);
+      } else {
+          NSLog(@"Data saved successfully.");
+      }
+  }];
+}
+
+- (IBAction)observe:(id)sender {
+  // Reference to the Firebase Database
+    FIRDatabaseReference *ref = [[[FIRDatabase database] reference] child:@"organizations/0"];
+
+    // Observe for data changes
+    [ref observeEventType:FIRDataEventTypeValue andPreviousSiblingKeyWithBlock:^(FIRDataSnapshot * _Nonnull snapshot, NSString * _Nullable prevKey) {
+        if (snapshot.value != [NSNull null]) {  // Check if the snapshot is not null
+            NSLog(@"Data: %@", snapshot.value);  // Log the data
+        }
+    } withCancelBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);  // Log any errors
+    }];
+}
+
+- (IBAction)getdata:(id)sender {
+  // Reference to the Firebase Database at the specific path
+     FIRDatabaseReference *ref = [[[FIRDatabase database] reference] child:@"organizations/0/data"];
+
+     // Perform a get request to retrieve the data
+     [ref getDataWithCompletionBlock:^(NSError * _Nullable error, FIRDataSnapshot * _Nullable snapshot) {
+         if (error) {
+             NSLog(@"Error getting data: %@", error.localizedDescription);
+         } else if (snapshot.exists) {
+             NSLog(@"Received data: %@", snapshot.value);
+         } else {
+             NSLog(@"No data available");
+         }
+     }];
+}
 
 @end
